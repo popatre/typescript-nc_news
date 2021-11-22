39a2dbbe243c14dae0838f0f373b989a9f015266
popatre <76402245+popatre@users.noreply.github.com>
2021-11-22 14:43:04 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+app.use(cors());
 const apiRouter = require("./routes/api-router");
 
 const {
